9655e70590a31ef520b6f0cffd7e4b1018f684c5
charity20 <mcharity980@gmail.com>
2022-12-14 17:12:18 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower -> prints alphabet other than c
+ *
+ * @c: parameter 1.
+ *
+ * Return: success
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z';
+}
