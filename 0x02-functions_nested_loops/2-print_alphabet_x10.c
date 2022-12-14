c47c5862c4308709ca2b7e9188a969df769d8ae3
charity20 <mcharity980@gmail.com>
2022-12-14 17:05:04 +0000

    alphabet x10 using nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints alphabet in 10 places
+ *
+ * prints_alphabet_x10 -> prints the lower case
+ *
+ * Return: Always 0 (success)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int loop = 0;
+
+	while (loop <= 10)
+
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+
+		loop++;
+	}
+}
