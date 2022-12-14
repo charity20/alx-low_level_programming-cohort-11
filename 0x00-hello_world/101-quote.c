a6a188098c4ea83bbf96e47c4b713ae5cb23d7bc
charity20 <mcharity980@gmail.com>
2022-12-14 00:14:23 +0000

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(2, "änd that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return (1);
+}
