e2ef4ebf1456746dda6ec7963a34003e2f03b25e
charity20 <mcharity980@gmail.com>
2022-12-15 22:24:15 +0000

    fixed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,6 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -28,6 +27,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	
 	return (largest);
 }
